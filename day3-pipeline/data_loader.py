40d481225232478028918f5371be5a5186c3d0d2
Trainer <trainer.official.edu@gmail.com>
2026-06-03 20:53:54 +0530

    day3 pipeline

---
+++ day3-pipeline/data_loader.py
@@ -0,0 +1,5 @@
+from sklearn.datasets import load_iris
+
+def load_data():
+    data = load_iris()
+    return data
---
+++ day3-pipeline/main.py
@@ -0,0 +1,21 @@
+from data_loader import load_data
+from preprocess import preprocess
+from train import train_model
+from evaluate import evaluate_model
+
+data = load_data()
+
+X_train, X_test, y_train, y_test = preprocess(data)
+
+model = train_model(
+    X_train,
+    y_train
+)
+
+accuracy = evaluate_model(
+    model,
+    X_test,
+    y_test
+)
+
+print(f"Accuracy: {accuracy}")
